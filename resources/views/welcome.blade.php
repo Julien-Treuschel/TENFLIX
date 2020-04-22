c3a4b113ed74f0975093dfe62e2f1e36e12948ae
Julien-Treuschel <33655640+Julien-Treuschel@users.noreply.github.com>
2020-04-22 09:20:41 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,221 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    
+      <div class="row" wfd-id="3">
+        
+        <div id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+            <div class="col-md-3" wfd-id="44">
+                <div class="card mb-3 shadow-sm" wfd-id="45">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="46">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="47">
+                        <div class="btn-group" wfd-id="48">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="72">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="71">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="39">
+                <div class="card mb-3 shadow-sm" wfd-id="40">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="41">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="42">
+                        <div class="btn-group" wfd-id="43">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="70">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="69">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>  
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="item">
+            <div class="col-md-3" wfd-id="44">
+                <div class="card mb-3 shadow-sm" wfd-id="45">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="46">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="47">
+                        <div class="btn-group" wfd-id="48">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="72">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="71">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="39">
+                <div class="card mb-3 shadow-sm" wfd-id="40">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="41">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="42">
+                        <div class="btn-group" wfd-id="43">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="70">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="69">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>  
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="item">
+            <div class="col-md-3" wfd-id="44">
+                <div class="card mb-3 shadow-sm" wfd-id="45">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="46">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="47">
+                        <div class="btn-group" wfd-id="48">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="72">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="71">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="39">
+                <div class="card mb-3 shadow-sm" wfd-id="40">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="41">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="42">
+                        <div class="btn-group" wfd-id="43">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="70">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="69">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>  
+                <div class="col-md-3" wfd-id="34">
+                <div class="card mb-3 shadow-sm" wfd-id="35">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body" wfd-id="36">
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center" wfd-id="37">
+                        <div class="btn-group" wfd-id="38">
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="68">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary" wfd-id="67">Edit</button>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>                 
+  
+    </div>
+</div>
+@endsection
